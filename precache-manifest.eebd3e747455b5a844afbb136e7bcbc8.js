dd654fb555634e646b507a04f5486bfb5e156f06
saurav1998hub <sauravagarwal104@gmail.com>
2020-05-19 16:49:24 +0530

    Updates

---
+++ precache-manifest.eebd3e747455b5a844afbb136e7bcbc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfb2e1c61923d044585bac00e351b50",
+    "url": "/CoronaToday/index.html"
+  },
+  {
+    "revision": "48ab94202761c2acfd01",
+    "url": "/CoronaToday/static/css/main.79ab9c26.chunk.css"
+  },
+  {
+    "revision": "7f26438f7a82355ee32b",
+    "url": "/CoronaToday/static/js/2.31870229.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoronaToday/static/js/2.31870229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ab94202761c2acfd01",
+    "url": "/CoronaToday/static/js/main.c7e8af50.chunk.js"
+  },
+  {
+    "revision": "568168043ca28ed2ba0d",
+    "url": "/CoronaToday/static/js/runtime-main.ba75e378.js"
+  }
+]);
